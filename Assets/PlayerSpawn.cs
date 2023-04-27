fb2ceab4be9b6bf5c123c35a2e999f9f18b430b9
Florian Maslowski <florian.maslowski@gmail.com>
2023-04-27 10:47:48 +0200

    commit

--- Assets/PlayerSpawn.cs
+++ Assets/PlayerSpawn.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour
 {
+    private float timer = 0;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +19,20 @@ public class PlayerSpawn : MonoBehaviour
         
     }
     // receives Player data and calculates if and where other Players are in the frame
-    private void spawnPlayer(int played_time, int yValue, int playerID)
+    private void spawnPlayer(List<Player> playerlist)
     {
-
+        foreach(Player player in playerlist)
+        {
+            if (player != null)
+            {
+                //GameObject newplayer = Instantiate(player);
+                //float x_value = (player.getPlaytime() - Time.realtimeSinceStartup) * 1;
+                ////Pipe spawns at height from data/server
+                //newplayer.transform.position = new Vector3(x_value, player.getHeight()f, 0);
+                ////Destroy player after every frame
+                //Destroy(newpipe, /*??*/);
+                
+            }
+        }
     }
 }
