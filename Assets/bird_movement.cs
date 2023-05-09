6db2bd1a2563484d34e4d90df553cd7dface8675
Drenis Rreci <drenisrreci@outlook.de>
2023-05-09 11:07:20 +0200

    Balls and Sound Effect added

--- Assets/bird_movement.cs
+++ Assets/bird_movement.cs
@@ -7,12 +7,14 @@ public class bird_movement : MonoBehaviour
     public ManagerGame gm;
     public float velocity = 1;
     private Rigidbody2D rb;
+    AudioSource audioData;
 
     // Start is called before the first frame update
     void Start()
     {
         //Add rigibody to component
         rb = GetComponent<Rigidbody2D>();
+        audioData = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -35,5 +37,10 @@ public class bird_movement : MonoBehaviour
     {
         //call gameover function in game manager
         gm.GameOver();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("OUCH");
+        audioData.Play();
     }
 }
